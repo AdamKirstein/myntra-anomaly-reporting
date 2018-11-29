2cf4057bc4def39e35ffdf4150e03163024a407c
Adam Kirstein <ank2157@columbia.edu>
2018-11-29 14:32:11 +0530

    code update

--- autoreporting.py
+++ autoreporting.py
@@ -1,4 +1,20 @@
+import pandas as pd
 from jinja2 import FileSystemLoader, Environment
+
+# Allow for very wide columns - otherwise columns are spaced and ellipse'd
+pd.set_option("display.max_colwidth", 200)
+
+
+def csv_to_html(filepath):
+    """
+    Open a .csv file and return it in HTML format.
+    :param filepath: Filepath to a .csv file to be read.
+    :return: String of HTML to be published.
+    """
+    df = pd.read_csv(filepath, index_col=0)
+    html = df.to_html()
+    return html
+
 
 # Configure Jinja and ready the loader
 env = Environment(
@@ -9,20 +25,6 @@ env = Environment(
 base_template = env.get_template("report.html")
 table_section_template = env.get_template("table_section.html")
 
-# Content to be published
-title = "Model Report"
-sections = list()
-sections.append(table_section_template.render(
-    model="VGG19",
-    dataset="VGG19_results.csv",
-    table="Table goes here."
-))
-sections.append(table_section_template.render(
-    model="MobileNet",
-    dataset="MobileNet_results.csv",
-    table="Table goes here."
-))
-
 
 def main():
     """
@@ -30,11 +32,26 @@ def main():
     Render a template and write it to file.
     :return:
     """
+
+    # Content to be published
+    title = "Model Report"
+    sections = list()
+    sections.append(table_section_template.render(
+        model="SARIMA",
+        dataset="revenue_data.csv",
+        table=csv_to_html("revenue_data.csv")
+    ))
+    sections.append(table_section_template.render(
+        model="Sarima",
+        dataset="revenue_data.csv.csv",
+        table=csv_to_html("revenue_data.csv")
+    ))
+
     with open("output/report.html", "w") as f:
-        f.write(base_template.render(
-            title=title,
-            sections=sections
-        ))
+            f.write(base_template.render(
+                title=title,
+                sections=sections
+            ))
 
 
 if __name__ == "__main__":
